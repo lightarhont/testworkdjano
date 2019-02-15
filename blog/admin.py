eaa53f2e2a5a048b67d3556f36ca0606bf205b85
Mikhail Nikiforov <mikha-nikiforov@yandex.ru>
2019-02-15 23:49:06 +0300

    Правка в адмике

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Post
+from .utils import mailsend 
 
 
 class AuthorAdmin(admin.ModelAdmin):
     
     fields = ['title', 'slug', 'introtext', 'fulltext']
-
+    
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        mailsend(request, obj.slug)
 
 admin.site.register(Post, AuthorAdmin)
